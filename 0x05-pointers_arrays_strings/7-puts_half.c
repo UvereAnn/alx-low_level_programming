482faa91ced64578abc3ea63c0567333f6505828
UvereAnn <uvereann@gmail.com>
2022-09-19 12:56:21 -0700

    Edited task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,25 +1,33 @@
-#include "main.h""
+#include "main.h"
 
 /**
- * puts_half - this function print only the even numbers.
- * @str: is the principal parameter.
+ * puts_half - prints half of a string
+ * followed by a new line
+ *  @str: string to be printed
  */
 void puts_half(char *str)
 {
-	int i, len, m;
+	int len, n, i;
 
-	for (len = 0; str[len] != '\0'; len++)
+	len = 0;
+
+	while (str[len] != '\0')
 	{
+		len++;
 	}
 
-	if ((len  % 2) == 0)
-		m = (len / 2);
-	else
-		m = (len + 1) / 2;
-
-	for (i = m; i < len; i++)
+	if (len % 2 == 0)
 	{
-		_putchar(str[i]);
+		for (i = len / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	} else if (len % 2)
+	{
+		for (n = (len - 1) / 2; n < len - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
 	}
 	_putchar('\n');
 }
